eaec3c47f92b62213fffbe8a77590f7b27d99b7a
Mason Crane <masonecrane@gmail.com>
2015-06-29 14:43:37 -0700

    didn't need an immediate function for expor. D'oh

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 var friends = require('./../server/controllers/friends.js');
 
-module.exports = (function(app) {
+module.exports = function(app) {
 	app.get('/friends', function(req, res) {
 		friends.show(req, res);
 	});
-})();
+}
